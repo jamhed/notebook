8b984f18b1158b6e0a9c1c3224f55cecc88730c7
Roman Galeev <jamhedd@gmail.com>
2020-01-10 00:44:46 +0100

    Skip cors

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8888
 c.NotebookApp.token = ''
 c.NotebookApp.base_url = os.getenv('BASE_URL', '/')
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.disable_check_xsrf = True
